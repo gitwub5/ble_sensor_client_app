3744ad023ce4eaec5c98ef22f85d07a116334303
GeonWoo SHIN <tonyw2@khu.ac.kr>
2025-04-02 11:08:09 +0900

    [Fix] Variable Name

--- lib/core/bluetooth/utils/ble_uuid.dart
+++ lib/core/bluetooth/utils/ble_uuid.dart
@@ -1,23 +1,22 @@
 import 'package:flutter_blue_plus/flutter_blue_plus.dart' as fb;
 
 // 필요 시 env 파일로 uuid 관리
-
 /// 📌 BLE UUID를 상수로 관리하는 클래스
 class BleUUID {
-  final String serviceUuidString = "5f97247b-4474-424c-a826-f8ec299b6937";
-  final String rxUuidString = "5f97247b-4474-424c-a826-f8ec299b6938";
-  final String txUuidString = "5f97247b-4474-424c-a826-f8ec299b6939";
+  final String serviceString = "5f97247b-4474-424c-a826-f8ec299b6937";
+  final String settingCharString = "5f97247b-4474-424c-a826-f8ec299b6938";
+  final String sensorDataCharString = "5f97247b-4474-424c-a826-f8ec299b6939";
 
   /// ✅ FlutterBluePlus에서 사용할 UUID 형식 변환
-  final fb.Guid serviceUuid;
-  final fb.Guid rxUuid;
-  final fb.Guid txUuid;
+  final fb.Guid service;
+  final fb.Guid setting;
+  final fb.Guid sensorData;
 
   /// 🔹 생성자에서 초기화
   BleUUID()
-      : serviceUuid = fb.Guid("5f97247b-4474-424c-a826-f8ec299b6937"),
-        rxUuid = fb.Guid("5f97247b-4474-424c-a826-f8ec299b6938"),
-        txUuid = fb.Guid("5f97247b-4474-424c-a826-f8ec299b6939");
+      : service = fb.Guid("5f97247b-4474-424c-a826-f8ec299b6937"),
+        setting = fb.Guid("5f97247b-4474-424c-a826-f8ec299b6938"),
+        sensorData = fb.Guid("5f97247b-4474-424c-a826-f8ec299b6939");
 
   /// 📌 특정 UUID가 유효한지 확인
   bool isValidUuid(String uuid) {
@@ -26,18 +25,3 @@ class BleUUID {
     return uuidRegex.hasMatch(uuid);
   }
 }
-
-/*
-// 사용 예시
-import 'utils/ble_uuid.dart';
-
-void main() {
-  print("Service UUID: ${BleUUID.serviceUuidString}");
-  print("RX UUID: ${BleUUID.rxUuidString}");
-  print("TX UUID: ${BleUUID.txUuidString}");
-
-  // ✅ UUID 유효성 검사
-  print(BleUUID.isValidUuid("5f97247b-4474-424c-a826-f8ec299b6937")); // true
-  print(BleUUID.isValidUuid("invalid-uuid")); // false
-}
-*/
