8488310f997ac7a19c567c4d51344954451c2d1f
GeonWoo SHIN <tonyw2@khu.ac.kr>
2025-04-10 11:13:36 +0900

    [Refactor] File Structure

--- lib/features/medicine_management/view/medicine_screen.dart
+++ lib/features/medicine_management/view/medicine_screen.dart
@@ -1,10 +1,11 @@
+import 'package:bluetooth_app/shared/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 
-class DatabaseScreen extends StatelessWidget {
+class MedicineScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("의약품 DB")),
+      appBar: CustomAppBar(title: "의약품 관리"),
       body: Center(child: Text("의약품 DB 페이지")),
     );
   }
