a77777961e59e275679fa413588ad06b5d1f335e
GeonWoo SHIN <tonyw2@khu.ac.kr>
2025-04-10 11:13:03 +0900

    [Refactor] File Structure

--- lib/features/fridge_management/view/fridge_screen.dart
+++ lib/features/fridge_management/view/fridge_screen.dart
@@ -1,10 +1,11 @@
+import 'package:bluetooth_app/shared/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 
 class FridgeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("냉장고 정보")),
+      appBar: CustomAppBar(title: "냉장고 정보"),
       body: Center(child: Text("냉장고 정보 페이지")),
     );
   }
